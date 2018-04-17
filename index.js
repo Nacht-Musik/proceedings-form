38177030053725783b5f9f9ddd0cb7271b26dbce
Kncht-Musik <to.akiyama.dev@gmail.com>
2018-04-17 15:21:44 +0900

    追加／削除ボタン　実装完了

--- index.js
+++ index.js
@@ -2,29 +2,32 @@ function init(){
   // 参加者追加('add-btn'をクリック)
   $('.add-btn').click(function(){
     // 追加する人の氏名を取得
-    var name_obj = document.getElementById("name_sel");
-    var idx = name_obj.selectedIndex;      //選択されている氏名のidを取得
-    var name_txt = name_obj.options[idx].text;    
-    console.log('氏名：' + name_txt + '  idx:' + idx);    //値取得確認用 
+    var name_txt = $('#name_sel option:selected').text();
 
     // 追加する人の役割を取得
-    var user_role_obj = document.getElementById("role_sel");
-    var idx = user_role_obj.selectedIndex;
-    var user_role_txt = user_role_obj.options[idx].text;
-    console.log('役割：' + user_role_txt + '  idx:' + idx);    //値取得確認用
+    var role_txt = $('#role_sel option:selected').text();
 
     var elm = $('.person')[0];
     elm = $(elm).clone().removeClass('template');
 
-    
-    $(elm).appendTo('.participants');
+    // 氏名及び役割の選択状態を識別
+    if (name_txt === "" || role_txt === "") {
+      alert('『氏名』及び『役割』を選択して下さい。');
+    } else {
+      // 該当箇所に選択中の氏名、役割を代入。
+      $(elm).find('.person_name').html(name_txt);
+      $(elm).find('.person_role').html(role_txt);
 
+      //加工した要素を追加
+      $(elm).appendTo('.participants');
+    }
 
   });
 
-  // del-btn クリック
-  $('.del-btn').click(function(){
-    console.log('del-btn click');
+  // 要素削除
+  $(document).on('click', '.del-btn', function(){
+    console.log($(this).text());
+    $(this).parents('.person').remove();
   });
 }
 
