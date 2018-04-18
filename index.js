ba8f1a9520415ce2048a1cf8008c900434b75c7f
Nacht-Musik <to.akiyama.dev@gmail.com>
2018-04-18 22:59:46 +0900

    refactoring & improb view design part.2

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 function init(){
   // 会議カテゴリーの選択
-  $('#meeting_category_sel').change(function() {
+  $('#meeting-category-sel').change(function() {
     // 選択オプションのvalueを取得
     var sel_val = $(this).val();
 
     if (sel_val === "open") {
       $('#meeting-form-1').removeClass('hidden');
-    } else if (sel_val === 'close') {
+    }else if (sel_val === 'close') {
       $('#meeting-form-1').addClass('hidden');
-    } else {
+    }else {
       console.log('想定外');
     }
   }); 
@@ -17,10 +17,10 @@ function init(){
   // 参加者追加('add-btn'をクリック)
   $('.add-btn').click(function(){
     // 追加する人の氏名を取得
-    var name = $('#name_sel option:selected').text();
+    var name = $('#name-sel option:selected').text();
 
     // 追加する人の役割を取得
-    var role = $('#role_sel option:selected').text();
+    var role = $('#role-sel option:selected').text();
 
     // 追加するHTML要素を取得(clone)し、非表示(template)クラスを削除
     var elm = $('.person')[0];
@@ -29,15 +29,15 @@ function init(){
     // 氏名及び役割の選択状態を識別
     if (name === "" && role === "") {
       $('#alert-disp').text('『氏名』及び『役割』を選択して下さい。');
-    } else if (name === "") {
+    }else if (name === "") {
       $('#alert-disp').text('『氏名』を選択して下さい。');
-    } else if (role === "") {
+    }else if (role === "") {
       $('#alert-disp').text('『役割』を選択して下さい。');
-    } else {
+    }else {
       $('#alert-disp').text('');
       // 該当箇所に選択中の氏名、役割を代入。
-      $(elm).find('.person_name').html(name);
-      $(elm).find('.person_role').html(role);
+      $(elm).find('.person-name').html(name);
+      $(elm).find('.person-role').html(role);
 
       //加工した要素を追加
       $(elm).appendTo('.participants');
